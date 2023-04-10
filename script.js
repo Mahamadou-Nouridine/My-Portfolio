bfeb41ed8c7dadf45a5a4ea64aabfbf55340ccab
Mahamadou-Nouridine <98597449+Mahamadou-Nouridine@users.noreply.github.com>
2023-04-10 17:19:13 +0100

    fix linters errors

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-const sideMenus = document.getElementsByClassName("side-menu");
-const hamburger = document.getElementById("hamburger");
-const closeMenu = document.getElementById("side-menu-close");
+const sideMenus = document.getElementsByClassName('side-menu');
+const hamburger = document.getElementById('hamburger');
+const closeMenu = document.getElementById('side-menu-close');
 const sideMenu = sideMenus[0];
 
 const openSideMenu = () => {
-  sideMenu.classList.add("open-side-menu");
+  sideMenu.classList.add('open-side-menu');
 };
 
 const closeSideMenu = () => {
-  sideMenu.classList.remove("open-side-menu");
+  sideMenu.classList.remove('open-side-menu');
 };
 
-hamburger.addEventListener("click", openSideMenu);
-closeMenu.addEventListener("click", closeSideMenu);
+hamburger.addEventListener('click', openSideMenu);
+closeMenu.addEventListener('click', closeSideMenu);
