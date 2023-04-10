898ee2622ee2cadc4267bc231e691de846f497ce
Shalini Nayan <117961613+ShaliniNayan@users.noreply.github.com>
2023-04-10 21:20:10 +0530

    Add close-menu and pointer

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 const sideMenus = document.getElementsByClassName("side-menu");
 const hamburger = document.getElementById("hamburger");
+const closeMenu = document.getElementById("side-menu-close");
 const sideMenu = sideMenus[0];
 
 const openSideMenu = () => {
   sideMenu.classList.add("open-side-menu");
 };
 
+const closeSideMenu = () => {
+  sideMenu.classList.remove("open-side-menu");
+};
+
 hamburger.addEventListener("click", openSideMenu);
+closeMenu.addEventListener("click", closeSideMenu);
