1d410795e2f211115c108dc452ddb45b939ed483
Mahamadou-Nouridine <98597449+Mahamadou-Nouridine@users.noreply.github.com>
2023-04-11 14:49:50 +0100

    fix issue https://github.com/Mahamadou-Nouridine/My-Portfolio/issues/9

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
-const sideMenu = document.querySelector(".side-menu");
-const hamburger = document.querySelector("#hamburger");
-const closeMenu = document.querySelector("#side-menu-close");
-const body = document.querySelector("body");
-const sideMenus = document.querySelectorAll("side-menu-item");
+const sideMenu = document.querySelector('.side-menu');
+const hamburger = document.querySelector('#hamburger');
+const closeMenu = document.querySelector('#side-menu-close');
+const body = document.querySelector('body');
+const sideMenus = document.querySelectorAll('.side-menu-item');
 
 const openSideMenu = () => {
-  sideMenu.classList.add("open-side-menu");
-  body.style.overflow = "hidden";
+  sideMenu.classList.add('open-side-menu');
+  body.style.overflow = 'hidden';
 };
 
 const closeSideMenu = () => {
-  sideMenu.classList.remove("open-side-menu");
-  body.style.overflowY = "unset";
+  sideMenu.classList.remove('open-side-menu');
+  body.style.overflowY = 'unset';
 };
 
-hamburger.addEventListener("click", openSideMenu);
-closeMenu.addEventListener("click", closeSideMenu);
+hamburger.addEventListener('click', openSideMenu);
+closeMenu.addEventListener('click', closeSideMenu);
 
 for (let i = 0; i < sideMenus.length; i += 1) {
-  sideMenus[i].addEventListener("click", closeSideMenu);
+  sideMenus[i].addEventListener('click', closeSideMenu);
 }
