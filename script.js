3414ea68669661d8f5cb78124790279f4f280358
Shalini Nayan <117961613+ShaliniNayan@users.noreply.github.com>
2023-04-11 18:56:35 +0530

    Add prevent background scrolling to side menu

--- script.js
+++ script.js
@@ -1,15 +1,22 @@
-const sideMenus = document.getElementsByClassName('side-menu');
-const hamburger = document.getElementById('hamburger');
-const closeMenu = document.getElementById('side-menu-close');
-const sideMenu = sideMenus[0];
+const sideMenu = document.querySelector(".side-menu");
+const hamburger = document.querySelector("#hamburger");
+const closeMenu = document.querySelector("#side-menu-close");
+const body = document.querySelector("body");
+const sideMenus = document.querySelectorAll("side-menu-item");
 
 const openSideMenu = () => {
-  sideMenu.classList.add('open-side-menu');
+  sideMenu.classList.add("open-side-menu");
+  body.style.overflow = "hidden";
 };
 
 const closeSideMenu = () => {
-  sideMenu.classList.remove('open-side-menu');
+  sideMenu.classList.remove("open-side-menu");
+  body.style.overflowY = "unset";
 };
 
-hamburger.addEventListener('click', openSideMenu);
-closeMenu.addEventListener('click', closeSideMenu);
+hamburger.addEventListener("click", openSideMenu);
+closeMenu.addEventListener("click", closeSideMenu);
+
+for (let i = 0; i < sideMenus.length; i += 1) {
+  sideMenus[i].addEventListener("click", closeSideMenu);
+}
